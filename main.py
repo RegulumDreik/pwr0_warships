c88f3e0390acd407e5e884d58b7d38fd3219f7a3
Dmitrii Kapustinskii <hunterelf1@gmail.com>
2023-05-23 22:25:54 +0200

    Added Game classes

--- main.py
+++ main.py
@@ -1,30 +1,99 @@
+import asyncio
 import sys
+import time
+from functools import partial
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from map import Map
 from ship import Ship
-from window import Ui_MainWindow
+from window import Ui_MainWindow, WarshipsGraphicsView
 
 
 class Player:
     name: str
     map: Map
     ships: list[Ship]
+    intell: list[Map.MapCell]
+    field: list[list[WarshipsGraphicsView]]
+
+    def __init__(
+        self,
+        name,
+        map,
+        ships,
+        intell,
+        field,
+    ):
+        self.name = name
+        self.map = map
+        self.ships = list(ships)
+        self.intell = list(intell)
+        self.field = field
+
+
+class Game:
+    active_player: Player
+    inactive_player: Player
+    ui_window: Ui_MainWindow
+    ui_app: QApplication
+
+    def __init__(
+        self,
+    ):
+        self.ui_window = Ui_MainWindow()
+        self.ui_app = QApplication(sys.argv)
+        window = QMainWindow()
+        window.setFixedSize(800, 600)
+        window.setWindowTitle("Warships")
+        window.show()
+        self.ui_window.setupUi(window)
+        self.active_player = Player(
+            name='Dodik',
+            map=Map(size=10),
+            ships=(),
+            intell=(),
+            field=self.ui_window.gridCells,
+        )
+        self.inactive_player = Player(
+            name='Dodik2',
+            map=Map(size=10),
+            ships=(),
+            intell=(),
+            field=self.ui_window.gridCells_2,
+        )
+        self.place_ships()
+        self.start_game()
+
+    def place_ships(self):
+        player1 = self.active_player
+        player1.ships.append(Ship(
+            (
+                player1.map.fields[1][2],
+                player1.map.fields[1][3],
+                player1.map.fields[1][4],
+            )
+        ))
+        player2 = self.inactive_player
+        player2.ships.append(Ship(
+            (
+                player2.map.fields[2][1],
+                player2.map.fields[3][1],
+                player2.map.fields[4][1],
+            )
+        ))
+
+    def start_game(self):
+        self.ui_window.drawShips(self.active_player.ships)
+        self.ui_window.drawIntell(self.active_player.intell)
+        for i in self.inactive_player.field:
+            for cell in i:
+                cell.instrument_function = self.make_turn
+
+    def make_turn(self, coordinates, parent, event, *args, **kwargs):
+        print(coordinates)
 
 
 if __name__ == '__main__':
-    map = Map(size=10)
-    Ship((map.fields[1][1], map.fields[1][2], map.fields[1][3]))
-    Ship((map.fields[5][4], map.fields[6][4], map.fields[7][4], map.fields[8][4]))
-
-    app = QApplication(sys.argv)
-    window = QMainWindow()
-    ui_windoes = Ui_MainWindow()
-    ui_windoes.setupUi(window)
-    window.setFixedSize(800, 600)
-    window.setWindowTitle("Warships")
-    window.show()
-    ui_windoes.DrawWarshipsFields(map, map)
-    app.exec_()
-
+    game = Game()
+    game.ui_app.exec_()
--- window.py
+++ window.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Collection, Callable, Optional
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QGraphicsGridLayout, QLabel, QGraphicsItem, \
     QPushButton, \
@@ -8,6 +9,7 @@ from PyQt5.QtGui import QPixmap, QColor, QPainter, QMouseEvent, QBrush
 from PyQt5 import QtGui
 
 from map import Map
+from ship import Ship
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
 SQUARE_SIDE = 20
@@ -15,15 +17,31 @@ ROWS, COLS = int(WINDOW_HEIGHT/SQUARE_SIDE), int(WINDOW_WIDTH/2*SQUARE_SIDE)
 
 class WarshipsGraphicsView(QGraphicsView):
 
+    coordinates: tuple[int, int]
+    instrument_function: Optional[Callable]
+
+    def __init__(self, coordinates, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instrument_function = None
+        self.coordinates = coordinates
+
     def mouseReleaseEvent(self, event: QMouseEvent) -> None:
         print('t')
         t = self.backgroundBrush()
         print(t.color().getRgb())
+        if self.instrument_function is None:
+            print('t1')
+        else:
+            self.instrument_function(coordinates=self.coordinates, parent=self.parent(), event=event)
+
+    def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
+        pass
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow, field_size: int = 10):
         size = int(min(WINDOW_WIDTH * 0.45, WINDOW_HEIGHT * 0.8))
+        self.main_window = MainWindow
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -31,10 +49,10 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.widget = QWidget(self.centralwidget)
         self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(20, 10, 761, 341))
+        self.widget.setGeometry(QRect(20, 10, 760, 340))
         self.horizontalLayoutWidget = QWidget(self.widget)
         self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
-        self.horizontalLayoutWidget.setGeometry(QRect(0, 0, 761, 341))
+        self.horizontalLayoutWidget.setGeometry(QRect(0, 0, 760, 340))
         self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.graphics_scene = QGraphicsScene(self.widget)
@@ -47,7 +65,7 @@ class Ui_MainWindow(object):
         for i in range(field_size):
             self.gridCells.append([])
             for y in range(field_size):
-                graphicsView = WarshipsGraphicsView(self.graphics_scene, self.widget)
+                graphicsView = WarshipsGraphicsView((i, y), self.graphics_scene, self.widget, )
                 graphicsView.setFixedSize(int(size / field_size), int(size / field_size))
                 self.gridLayout.addWidget(graphicsView, i, y, 1, 1)
                 self.gridCells[i].append(graphicsView)
@@ -62,25 +80,18 @@ class Ui_MainWindow(object):
         for i in range(field_size):
             self.gridCells_2.append([])
             for y in range(field_size):
-                graphicsView = WarshipsGraphicsView(self.graphics_scene, self.widget)
+                graphicsView = WarshipsGraphicsView((i, y), self.graphics_scene, self.widget)
                 graphicsView.setFixedSize(int(size / field_size), int(size / field_size))
                 self.gridLayout_2.addWidget(graphicsView, i, y, 1, 1)
-                self.gridCells[i].append(graphicsView)
+                self.gridCells_2[i].append(graphicsView)
 
         self.horizontalLayout.addLayout(self.gridLayout_2)
 
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(MainWindow)
-        self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 21))
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        QMetaObject.connectSlotsByName(MainWindow)  # I dunno what it doing.
     # setupUi
 
     def DrawWarshipsFields(self, current_player_map: Map, other_player_map: Map):
@@ -89,6 +100,35 @@ class Ui_MainWindow(object):
                 visual_object: WarshipsGraphicsView = self.gridCells[cell.coordinates[0]][cell.coordinates[1]]
                 if cell.entity is not None:
                     visual_object.setBackgroundBrush(QBrush(QColor(100, 100, 100)))
+
+    def drawShips(self, ships: Collection[Ship], target_field=None):
+        if target_field is None:
+            target_field = self.gridCells
+        for ship in ships:
+            for cell in ship.position_cells:
+                visual_object: WarshipsGraphicsView = target_field[cell.coordinates[0]][cell.coordinates[1]]
+                if ship.destroyed:
+                    visual_object.setBackgroundBrush(QBrush(QColor(173, 37, 3)))
+                    continue
+                if cell in ship.damaged_cells:
+                    visual_object.setBackgroundBrush(QBrush(QColor(255, 174, 66)))
+                    continue
+                visual_object.setBackgroundBrush(QBrush(QColor(100, 100, 100)))
+
+    def drawIntell(self, intell: Collection[Map.MapCell], target_field=None):
+        if target_field is None:
+            target_field = self.gridCells_2
+        for spy in intell:
+            visual_object: WarshipsGraphicsView = target_field[spy.coordinates[0]][spy.coordinates[1]]
+            if spy.entity is None:
+                visual_object.setBackgroundBrush(QBrush(QColor(3, 99, 173)))
+                continue
+            if spy.entity.destroyed:
+                visual_object.setBackgroundBrush(QBrush(QColor(173, 37, 3)))
+                continue
+            if spy in spy.entity.damaged_cells:
+                visual_object.setBackgroundBrush(QBrush(QColor(255, 174, 66)))
+                continue
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
